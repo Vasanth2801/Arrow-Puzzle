c6553c5d34161e21c3182185f2d4587de6b19f58
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-08-20 18:36:49 +0530

    Level Selector Script

--- Arrow Puzzle/Assets/Scripts/LevelSelectorManager.cs
+++ Arrow Puzzle/Assets/Scripts/LevelSelectorManager.cs
@@ -20,7 +20,71 @@ public class LevelSelectorManager : MonoBehaviour
         {
             totalLevels = levelManager.GetTotallevelCount();
         }
+
+        RefreshPage();
     }
 
+    private void RefreshPage()
+    {
+        int startLevel = currentPage * levelPerPage;
 
+        for(int i =0; i < levelButtons.Length; i++)
+        {
+            int levelNumber = startLevel + i + 1;
+
+            Button button = levelButtons[i];
+
+            if(levelNumber > totalLevels)
+            {
+                button.gameObject.SetActive(false);
+                continue;
+            }
+
+            button.gameObject.SetActive(true);
+
+            button.interactable = levelNumber <= GetUnlockedLevel();
+
+            int selectedLevel = levelNumber;
+
+            button.onClick.RemoveAllListeners();
+
+            button.onClick.AddListener(() => LoadLevel(selectedLevel));
+        }
+    }
+
+    private int GetUnlockedLevel()
+    {
+        return PlayerPrefs.GetInt("unlockedLevel", 1);
+    }
+
+    private void LoadLevel(int levelNumber)
+    {
+        PlayerPrefs.SetInt("selectedLevel", levelNumber);
+
+        PlayerPrefs.Save();
+
+        Debug.Log("Selected Level: " + levelNumber);
+    }
+
+    public void NextPage()
+    {
+        int maxPage = Mathf.CeilToInt((float)totalLevels/ levelPerPage) -1;
+
+        if(currentPage < maxPage)
+        {
+            currentPage++;
+
+            RefreshPage();
+        }
+    }
+
+    public void PreviousPage()
+    {
+        if(currentPage > 0)
+        {
+            currentPage--;
+
+            RefreshPage();
+        }
+    }
 }
