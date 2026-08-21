dbd040ac3780301978eb7ab492528ddaf47bf1d3
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-08-21 16:17:47 +0530

    Level Button Script

--- Arrow Puzzle/Assets/Scripts/LevelButton.cs
+++ Arrow Puzzle/Assets/Scripts/LevelButton.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelButton : MonoBehaviour
 {
-    
+    [SerializeField] private Button button;
+
+    private int levelNumber;
+
+    public void Setup(int level)
+    {
+        levelNumber = level;
+
+        if(button == null)
+        {
+            button = GetComponent<Button>();
+        }
+
+        button.onClick.RemoveAllListeners();
+
+        button.onClick.AddListener(OpenLevel);
+    }
+
+    private void OpenLevel()
+    {
+        if(LevelManager.instance == null)
+        {
+            Debug.LogError("LevelManager not found");
+            return;
+        }
+
+        LevelManager.instance.LoadLevel(levelNumber);
+    }
 }
